5aa0e16ad1db1b1434eef851840d2d0f92d2dd4e
Enock-back-end <enockshabangu2028@gmail.com>
2025-05-23 14:17:24 +0200

    add basic arithemetic script

---
+++ basic_operations.py
@@ -0,0 +1,14 @@
+# Define two numbers
+number1 = 10
+number2 = 5
+
+# basic arithmetic operations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Print the results 
+print("Addition of", number1, "and", number2, "is", sum_result)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
+
