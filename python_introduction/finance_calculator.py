c268bcd8c9d47f8fb5db3c215ee3be8fde45ebff
Enock-back-end <enockshabangu2028@gmail.com>
2025-05-23 15:51:54 +0200

    Fix calculation of monthly_savings and projected_savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,17 +1,8 @@
-# Prompt the user for monthly income
-income = float(input("Enter your monthly income: "))
+monthly_income = input("Enter your monthly income: ")
+monthly_expenses = input("Enter your total monthly expenses: ")
 
-# Prompt the user for monthly expenses
-expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = float(monthly_income) - float(monthly_expenses)
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-# Calculate monthly savings
-monthly_savings = income - expenses
-
-# Calculate projected annual savings with 5% interest
-annual_savings = monthly_savings * 12
-interest = annual_savings * 0.05
-projected_savings = annual_savings + interest
-
-# Output results
 print(f"Your monthly savings are ${monthly_savings}.")
 print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
