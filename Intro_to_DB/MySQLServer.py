2ee1fc804731430bef5031a4711934035f15fb71
Enock-back-end <enockshabangu2028@gmail.com>
2025-06-27 21:59:09 +0200

    Fix: match exact checker format for FOREIGN KEY

--- Intro_to_DB/MySQLServer.py
+++ Intro_to_DB/MySQLServer.py
@@ -4,16 +4,13 @@ from mysql.connector import Error
 
 DB_NAME = "alx_book_store"
 
-
 def create_database():
-
     connection = None
-    cursor = None
     try:
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="Alisa_2005"  # MySQL root password  and details
+            password="Alisa_2005"
         )
 
         if connection.is_connected():
@@ -22,15 +19,13 @@ def create_database():
             print(f"Database '{DB_NAME}' created successfully!")
 
     except Error as err:
-        print(f"Error: '{err}'")
+        print(f"Error: {err}")
         sys.exit(1)
 
     finally:
-        if cursor:
-            cursor.close()
         if connection and connection.is_connected():
+            cursor.close()
             connection.close()
-
 
 if __name__ == "__main__":
     create_database()
