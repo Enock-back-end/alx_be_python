98713824610bd995ec13cbc963de646a03ac2616
Enock-back-end <enockshabangu2028@gmail.com>
2025-06-28 00:00:27 +0200

    Update task_5.sql

--- Intro_to_DB/task_5.sql
+++ Intro_to_DB/task_5.sql
@@ -1,15 +1,11 @@
-USE alx_book_store;
-
-INSERT IGNORE INTO
-    customers (
-        customer_id,
-        customer_name,
-        email,
-        address
-    )
-VALUES (
-        1,
-        'Cole Baidoo',
-        'cbaidoo@sandtech.com',
-        '123 Happiness Ave.'
-    );
+INSERT INTO customers (
+    customer_id,
+    customer_name,
+    email,
+    address
+) VALUES (
+    1,
+    'Cole Baidoo',
+    'cbaidoo@sandtech.com',
+    '123 Happiness Ave.'
+);
